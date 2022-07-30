b85cd1b2f90ddfa2d4e3bf967f554ac60eef9a4b
hiteshchoudhary <hitesh@hiteshchoudhary.com>
2022-07-30 21:41:18 +0530

    new shopping app project added

---
+++ shoppingApp05/shoppingApp05/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  shoppingApp05
+//
+//  Created by Hitesh Choudhary on 30/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ shoppingApp05/shoppingApp05/shoppingApp05App.swift
@@ -0,0 +1,17 @@
+//
+//  shoppingApp05App.swift
+//  shoppingApp05
+//
+//  Created by Hitesh Choudhary on 30/07/22.
+//
+
+import SwiftUI
+
+@main
+struct shoppingApp05App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
