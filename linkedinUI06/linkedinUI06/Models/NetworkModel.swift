2540f58f0e16d57be43c8eb6941f49b50e9cd3c5
hiteshchoudhary <hitesh@hiteshchoudhary.com>
2022-08-08 18:20:09 +0530

    added models in linkedin ui

---
+++ linkedinUI06/linkedinUI06/Models/NetworkModel.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkModel.swift
+//  linkedinUI06
+//
+//  Created by Hitesh Choudhary on 08/08/22.
+//
+
+import Foundation
+
+struct NetworkModel {
+    var id: Int
+    var name: String
+    var position: String
+    var mutual: String
+    var image: String
+}
---
+++ linkedinUI06/linkedinUI06/Models/PostModel.swift
@@ -0,0 +1,16 @@
+//
+//  PostModel.swift
+//  linkedinUI06
+//
+//  Created by Hitesh Choudhary on 08/08/22.
+//
+
+import Foundation
+
+struct PostData {
+    var id: Int
+    var Image: String
+    var title: String
+    var followers: Int
+    var profileImage: String
+}
