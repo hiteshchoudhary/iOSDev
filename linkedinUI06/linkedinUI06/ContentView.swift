8306bd3a8780e66a116888de582e832915f71fa5
hiteshchoudhary <hitesh@hiteshchoudhary.com>
2022-08-08 13:54:48 +0530

    added linkedin ui project

---
+++ linkedinUI06/linkedinUI06/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  linkedinUI06
+//
+//  Created by Hitesh Choudhary on 08/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ linkedinUI06/linkedinUI06/linkedinUI06App.swift
@@ -0,0 +1,17 @@
+//
+//  linkedinUI06App.swift
+//  linkedinUI06
+//
+//  Created by Hitesh Choudhary on 08/08/22.
+//
+
+import SwiftUI
+
+@main
+struct linkedinUI06App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
